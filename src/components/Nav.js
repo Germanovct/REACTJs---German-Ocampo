4cd72474f241d453da14bf2d20ed5603eb50be7c
germano <germanovct@gmail.com>
2022-08-21 21:55:13 -0300

    JSX

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+export const Nav = () => {
+
+    return(
+        <div  className="navbar">
+        <header>
+        <h1>VCT RECORDS</h1>
+    <nav>
+        <a>Home</a>
+        <a>Releases</a>
+        <a>Soundcloud</a>
+    </nav>
+    </header>
+    </div>
+    )
+
+}
